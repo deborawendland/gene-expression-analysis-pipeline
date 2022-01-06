71ba0b233fd77f6ae186bdc2e031b9e875264624
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-01-06 16:46:32 -0300

    struct: update filenames

--- Pipeline/DockerConfig/install_basic_requirements.sh
+++ Pipeline/DockerConfig/install_basic_requirements.sh
@@ -9,4 +9,4 @@ apt install vim -y
 apt-get install g++ -y
 apt-get install make -y
 apt install zlib1g-dev -y
-# apt-get install libbz2-dev
+apt-get install libbz2-dev -y
--- Pipeline/DockerConfig/samtools.sh
+++
@@ -1,1 +0,0 @@
-apt-get install samtool -y

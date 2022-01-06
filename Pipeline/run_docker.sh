32f0631db353a0366ea4f09f6e9aa8a23efc7d82
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-01-06 17:31:00 -0300

    minor fixes

--- Pipeline/run_docker.sh
+++ Pipeline/run_docker.sh
@@ -1,6 +1,5 @@
 docker container stop pipeline
 docker container rm pipeline
 docker container ls -a
-docker build --tag pipeline .
-# docker run -p 5000:5000 -d -it --name pipeline pipeline
-docker run -it --name pipeline pipeline
+docker build --tag deborawendland/gene-expression-pipeline:dev .
+docker run -it --name pipeline deborawendland/gene-expression-pipeline:dev

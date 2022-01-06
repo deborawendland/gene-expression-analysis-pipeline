71e00c30a9205541e86b8d10d7454737d91eb3f9
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-01-06 15:11:56 -0300

    build debug: divided build req and installations

---
+++ Pipeline/DockerConfig/basic_requirements.sh
@@ -0,0 +1,8 @@
+# install and update basic ubuntu packages on docker image 
+apt update -y
+apt upgrade -y
+apt-get install software-properties-common -y
+apt install wget -y
+apt install vim -y
+apt-get install g++ -y
+apt-get install make -y
---
+++ Pipeline/DockerConfig/star_requirement.sh
@@ -0,0 +1,5 @@
+wget https://github.com/alexdobin/STAR/archive/2.7.9a.tar.gz
+tar -xzf 2.7.9a.tar.gz
+cd STAR-2.7.9a
+cd STAR/source
+make STAR

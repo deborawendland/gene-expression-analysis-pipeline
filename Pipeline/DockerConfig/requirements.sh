d70c01452a456103db7abb97481e0166e7e486f3
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-01-06 14:28:58 -0300

    build: add docker with pipeline programs

--- Pipeline/DockerConfig/requirements.sh
+++ Pipeline/DockerConfig/requirements.sh
@@ -1,20 +1,31 @@
-# install basic ubuntu packages on docker image 
+# install and update basic ubuntu packages on docker image 
 apt update -y
 apt upgrade -y
-apt-get install software-properties-common-y
+apt-get install software-properties-common -y
 apt install wget -y
 apt install vim -y
+apt-get install g++ -y
+apt-get install make -y
 
 # programs
-apt install fastqc
+apt install fastqc -y
+apt install trimmomatic -y
+
+wget https://github.com/alexdobin/STAR/archive/2.7.9a.tar.gz
+tar -xzf 2.7.9a.tar.gz
+cd STAR-2.7.9a
+cd STAR/source
+make STAR
+
+apt-get install samtool -y
 
 # others
 
 # apt-get install python3 -y
-# apt-get -y install python3-pip
-# apt update -y
-# apt upgrade -y
-# apt-get install r-base -y
+# apt-get install python3-pip -y
+apt update -y
+apt upgrade -y
+apt-get install r-base -y
 
 # dependencies
 

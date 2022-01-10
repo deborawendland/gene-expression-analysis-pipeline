afc1b84754ecf9ffc362cb2301626b5039603545
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-01-09 21:11:18 -0300

    struct: update template folder

--- Scripts/run_multiqc.sh
+++ Scripts/run_multiqc.sh
@@ -1,5 +1,10 @@
 INPUT_PATH="/root/Analysis/Fastqc"
-OUTPUT_PATH="/root/Analysis/Multiqc"
+OUTPUT_PATH="/root/Scripts/multiqc_report"
 OUTPUT_FILENAME="multiqc_report.html"
 
 multiqc ${INPUT_PATH}
+
+mv ${OUTPUT_FILENAME} ${OUTPUT_PATH}/templates/${OUTPUT_FILENAME}
+RUN mv multiqc_data ${OUTPUT_PATH}/templates/multiqc_data
+
+# FLASK_APP=${OUTPUT_PATH}/app.py flask run --host=0.0.0.0 

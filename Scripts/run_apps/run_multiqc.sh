99d2d9c692162132c8a0350f10c2b5f1b1e315c1
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-01-09 22:21:48 -0300

    analysis: add trimmomatic to multiqc report

--- Scripts/run_apps/run_multiqc.sh
+++ Scripts/run_apps/run_multiqc.sh
@@ -3,9 +3,10 @@ TRIMMOMATIC_PATH="/root/Analysis/Trimmomatic"
 OUTPUT_PATH="/root/Scripts/multiqc_report"
 OUTPUT_FILENAME="multiqc_report.html"
 
-multiqc ${FASTQC_PATH}
+multiqc ${FASTQC_PATH} ${TRIMMOMATIC_PATH}
 
 mv ${OUTPUT_FILENAME} ${OUTPUT_PATH}/templates/${OUTPUT_FILENAME}
 mv multiqc_data ${OUTPUT_PATH}/templates/multiqc_data
 
-# FLASK_APP=${OUTPUT_PATH}/app.py flask run --host=0.0.0.0 
+# FLASK_APP=${OUTPUT_PATH}/app.py flask run --host=0.0.0.0
+# FLASK_APP=/root/Scripts/multiqc_report/app.py flask run --host=0.0.0.0

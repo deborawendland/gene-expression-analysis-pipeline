dd12c3acc6b8cd4184d2ad25962c0d6ca3606224
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-01-11 14:01:19 -0300

    minor fixes

---
+++ Scripts/run_apps/run_all.sh
@@ -0,0 +1,3 @@
+sh /root/Scripts/run_apps/run_fastqc.sh
+sh /root/Scripts/run_apps/run_trimmomatic.sh
+sh /root/Scripts/run_apps/run_multiqc.sh
--- run_docker.sh
+++ run_docker.sh
@@ -6,3 +6,5 @@ docker build --tag deborawendland/gene-expression-pipeline:analysis .
 # docker run -p 5000:5000 -d -it --name gene-analysis analysis-pipeline
 # docker run -it --name analysis deborawendland/gene-expression-pipeline:analysis
 docker run -p 8080:5000 --name analysis deborawendland/gene-expression-pipeline:analysis
+
+docker run -it -v E:\gene-expression-analysis-pipeline\Data:/root/Data --name analysis deborawendland/gene-expression-pipeline:analysis

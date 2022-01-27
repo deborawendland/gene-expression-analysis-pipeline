6016ba071e05b24327934adbcde29686ba940d0c
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-01-27 17:28:32 -0300

    installation update files

--- local_install_all.sh
+++ local_install_all.sh
@@ -4,9 +4,11 @@ sudo sh Pipeline/DockerConfig/install_python_dependencies.sh
 sudo sh Pipeline/DockerConfig/install_fastqc.sh
 sudo sh Pipeline/DockerConfig/install_trimmomatic.sh
 sudo sh Pipeline/DockerConfig/install_multiqc.sh
-sudo sh Pipeline/DockerConfig/install_star.sh
 sudo sh Pipeline/DockerConfig/install_samtools.sh
 sudo sh Pipeline/DockerConfig/install_r.sh
+sudo sh Pipeline/DockerConfig/install_bowtie2.sh
+sudo sh Pipeline/DockerConfig/install_feature_counts.sh
+sudo sh Pipeline/DockerConfig/install_seqtk.sh
 
 sudo R < Pipeline/DockerConfig/install_dependencies.R --no-save
 sudo R < Pipeline/DockerConfig/install_biocmanager.R --no-save

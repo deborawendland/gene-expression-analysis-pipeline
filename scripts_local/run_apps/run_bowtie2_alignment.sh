372b6cbf737ee3d6b5c461a8afd8c37c9536fc30
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-01-27 17:54:32 -0300

    separete stages of alignment

---
+++ scripts_local/run_apps/run_bowtie2_alignment.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+INPUT_PATH="./Analysis/Seqtk"
+DATABASE="GRCh38"
+OUTPUT_PATH="./Analysis/Bowtie2"
+
+OUTPUT_GEN_DIR="${OUTPUT_PATH}/genome_dir"
+OUTPUT_ALIGN="${OUTPUT_PATH}/alignment"
+mkdir ${OUTPUT_ALIGN}
+
+# inspect
+./bowtie2-2.4.5-linux-x86_64/bowtie2-inspect -n ${OUTPUT_GEN_DIR}/${DATABASE} 
+
+# align
+for file in $(ls $INPUT_PATH)
+do
+    SAMPLE=`basename $file`
+    echo "Alignment of sample: ${SAMPLE}..."
+    ./bowtie2-2.4.5-linux-x86_64/bowtie2 \
+        --local \
+        --very-fast-local \
+        -x ${OUTPUT_GEN_DIR}/${DATABASE} \
+        -U ${INPUT_PATH}/${SAMPLE} \
+        -S ${OUTPUT_ALIGN}/${SAMPLE%.*}.sam \
+        --no-unal \
+        --threads 6
+done
---
+++ scripts_local/run_apps/run_bowtie2_index.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+REFERENCE_GENOME_FILENAME="GCF_000001405.39_GRCh38.p13_genomic.fna"
+REF_PATH="./Data/reference_genome_hg38"
+
+OUTPUT_PATH="./Analysis/Bowtie2"
+OUTPUT_GEN_DIR="${OUTPUT_PATH}/genome_dir"
+
+mkdir ${OUTPUT_PATH}
+mkdir ${OUTPUT_GEN_DIR}
+
+# create indexes
+./bowtie2-2.4.5-linux-x86_64/bowtie2-build \
+    --threads 6 \
+    ${REF_PATH}/${REFERENCE_GENOME_FILENAME} \
+    ${OUTPUT_GEN_DIR}/${DATABASE} 

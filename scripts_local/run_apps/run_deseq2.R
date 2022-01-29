79c1b29e23548fd2ac5c16d2ca7c8fd9c6d4109b
Jacques <viniciusjacques@gmail.com>
2022-01-29 00:40:51 -0300

    fix dependencies, export results, dataframes index

--- scripts_local/run_apps/run_deseq2.R
+++ scripts_local/run_apps/run_deseq2.R
@@ -3,13 +3,15 @@
 #source("https://bioconductor.org/biocLite.R")
 #biocLite("DESeq2")
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-install.packages("BiocManager")
-BiocManager::install("Rsubread")
-BiocManager::install("DESeq2")
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#install.packages("BiocManager")
+#BiocManager::install("Rsubread")
+#BiocManager::install("DESeq2")
 
 library("DESeq2")
 library(Rsubread)
+
+require(dplyr)
 
 # DESEQ2
 output.path <- "./Analysis/DESeq2"
@@ -23,6 +25,14 @@ metadata <- metadata[,c("Age","gender","status")]
 colnames(fc) <- sub(".bam", "", colnames(fc))
 colnames(fc) <- sub("X", "", colnames(fc))
 
+# colnames(fc)
+# rownames(metadata)
+
+rownames(metadata) <- seq.int(2, nrow(metadata)+1)
+
+# colnames(fc)
+# rownames(metadata)
+
 fc <- fc[,rownames(metadata)]
 all(rownames(metadata) %in% colnames(fc))
 
@@ -35,3 +45,18 @@ dds <- DESeq(dds)
 dds 
 
 resultsNames(dds) # lists the coefficients
+res <- results(dds, tidy=TRUE)
+
+head(res) #let's look at the results table
+summary(res) #summary of results
+
+res <- res[order(res$padj),]
+head(res)
+
+write.table(
+  x=data.frame(res,
+    stringsAsFactors=FALSE),
+  file="./Analysis/DESeq2/results.csv",
+  quote=FALSE,
+  sep=",",
+  row.names=FALSE)
--- scripts_local/run_apps/run_deseq2.sh
+++ scripts_local/run_apps/run_deseq2.sh
@@ -8,9 +8,12 @@ DESEQ_PATH="./Analysis/Deseq2"
 mkdir ${COUNTS_PATH}
 mkdir ${DESEQ_PATH}
 
-sudo apt-get update -y
-sudo apt-get install libxml2-dev -y
-sudo apt-get install r-cran-xml -y
-sudo apt-get install libcurl4-openssl-dev -y
+# Rename metadata
+mv ./Data/data_SRP316381/SraRunTable.txt ./Data/data_SRP316381/SraRunTable.csv
+
+#sudo apt-get update -y
+#sudo apt-get install libxml2-dev -y
+#sudo apt-get install r-cran-xml -y
+#sudo apt-get install libcurl4-openssl-dev -y
 
 R < ./scripts_local/run_apps/run_deseq2.R --no-save

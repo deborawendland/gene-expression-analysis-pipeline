d095785bca7854e5641729ccba2c5d8a94142023
Jacques <viniciusjacques@gmail.com>
2022-01-29 00:39:32 -0300

    build: rm image

--- Data/publish_image.sh
+++
@@ -1,2 +0,0 @@
-docker tag analysis-pipeline-data deborawendland/gene-expression-data:data-sample
-# docker push deborawendland/gene-expression-data:data-sample
--- Data/run_docker.sh
+++
@@ -1,8 +0,0 @@
-docker container stop pipeline-data
-docker container rm pipeline-data
-docker container ls -a
-docker build --tag deborawendland/gene-expression-pipeline:data-sample .
-docker run -it --name pipeline-data deborawendland/gene-expression-pipeline:data-sample
-#docker run -i --name pipeline-data pipeline-data
-
-docker push deborawendland/gene-expression-pipeline:data-samples

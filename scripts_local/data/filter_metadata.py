271a350f2f67d0b5553840c80336556090a26a11
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-02-01 09:38:09 -0300

    data: add metadata table generator

---
+++ scripts_local/data/filter_metadata.py
@@ -0,0 +1,19 @@
+from curses import meta
+import pandas as pd
+import numpy as np
+
+metadata_export_columns = ['sample','status']
+
+run_table = pd.read_csv('./Data/data_SRP316381/SraRunTable.csv')
+
+run_table['sample'] = np.arange(2, len(run_table['status']) + 2)
+metadata = run_table[run_table.columns.intersection(metadata_export_columns)]
+
+cols = metadata.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+metadata = metadata[cols]
+
+metadata.loc[metadata['status'] == 'A', ['status']] = 'Asymptomatic'
+metadata.loc[metadata['status'] == 'S', ['status']] = 'Symptomatic'
+
+metadata.to_csv('./Data/data_SRP316381/metadata.csv', encoding='utf-8', index=False)
---
+++ scripts_local/data/run_metadata.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python3-pandas -y
+sudo pip install numpy
+
+python3 ./scripts_local/data/filter_metadata.py

48c845c3adc464a53874eb2c3aa5fac987be8661
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-02-03 11:44:28 -0300

    rm installation step

--- scripts_local/run_apps/run_deseq2.sh
+++ scripts_local/run_apps/run_deseq2.sh
@@ -11,9 +11,4 @@ mkdir ${DESEQ_PATH}
 # Rename metadata
 mv ./Data/data_SRP316381/SraRunTable.txt ./Data/data_SRP316381/SraRunTable.csv
 
-#sudo apt-get update -y
-#sudo apt-get install libxml2-dev -y
-#sudo apt-get install r-cran-xml -y
-#sudo apt-get install libcurl4-openssl-dev -y
-
 R < ./scripts_local/run_apps/run_deseq2.R --no-save
--- scripts_local/run_apps/run_transcriptogramer.sh
+++ scripts_local/run_apps/run_transcriptogramer.sh
@@ -2,9 +2,12 @@
 
 echo "Running Transcriptogramer"
 
-INPUT_PATH="./Analysis/"
+INPUT_PATH="./Analysis/FeatureCounts"
 OUTPUT_PATH="./Analysis/Transcriptogramer"
 
 mkdir ${OUTPUT_PATH}
+
+# convert from txt to csv
+# tr ' ' ',' <./Data/reference_proteins/9606.protein.links.v11.5.txt >./Data/reference_proteins/9606.protein.links.v11.5.csv 
 
 R < ./scripts_local/run_apps/run_transcriptogramer.R --no-save

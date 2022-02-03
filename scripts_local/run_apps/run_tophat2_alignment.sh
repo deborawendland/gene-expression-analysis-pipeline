af871601337c64285b44290ac6ca7117edc2ce6c
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-02-03 10:51:26 -0300

    update parameters

--- scripts_local/run_apps/run_tophat2_alignment.sh
+++ scripts_local/run_apps/run_tophat2_alignment.sh
@@ -15,24 +15,24 @@ rm -r ${TOPHAT_DIR}
 mkdir ${TOPHAT_DIR}
 mkdir ${TOPHAT_DIR_ALING}
 
-
 # inspect
-./bowtie2-2.4.5-linux-x86_64/bowtie2-inspect -n ${INPUT_PATH_GEN_DIR}/${DATABASE} 
+# ./bowtie2-2.4.5-linux-x86_64/bowtie2-inspect -n ${INPUT_PATH_GEN_DIR}/${DATABASE} 
 
 # alignment
 for file in $(ls $INPUT_FASTQ_PATH)
 do
     SAMPLE=`basename $file`
     
-    if [ -e ${TOPHAT_DIR_ALING}/${SAMPLE%.*}.sam ]; then
-        echo "File exists: ${SAMPLE%.*}.sam"
+    if [ -d ${TOPHAT_DIR_ALING}/${SAMPLE%.*} ]; then
+        echo "Directory exists: ${SAMPLE%.*}"
     else
         echo "Alignment of sample: ${SAMPLE}..."
         ./Older/tophat-2.1.1.Linux_x86_64/tophat2 \
-            --output-dir ${TOPHAT_DIR_ALING} \
+            --output-dir ${TOPHAT_DIR_ALING}/${SAMPLE%.*} \
+            --library-type fr-firststrand \
+            --num-threads 4 \
             --max-multihits 1 \
             ${INPUT_PATH_GEN_DIR}/${DATABASE} \
-            ${INPUT_FASTQ_PATH}/${SAMPLE} \
-            --library-type fr-firststrand #library-type precisa confirmar pelo tipo de sequenciament
+            ${INPUT_FASTQ_PATH}/${SAMPLE} 
     fi
 done
--- scripts_local/run_apps/run_trimmomatic.sh
+++ scripts_local/run_apps/run_trimmomatic.sh
@@ -3,7 +3,7 @@
 echo "Running Trimmomatic"
 
 # INPUT_PATH="./Data/data_SRP316381"
-INPUT_PATH="./Analysis/Seqtk"
+INPUT_PATH="./Analysis/Seqtk_2"
 OUTPUT_PATH="./Analysis/Trimmomatic_2"
 mkdir ${OUTPUT_PATH}
 
@@ -14,7 +14,8 @@ do
     if [ -e ${OUTPUT_PATH}/${SAMPLE} ]; then
         echo "File exists: ${SAMPLE}"
     else
-        java -jar /usr/share/java/trimmomatic-0.39.jar SE \
+        java -jar /usr/share/java/trimmomatic-0.39.jar \
+            SE \
             -threads 4 \
             ${INPUT_PATH}/${SAMPLE} \
             ${OUTPUT_PATH}/${SAMPLE} \

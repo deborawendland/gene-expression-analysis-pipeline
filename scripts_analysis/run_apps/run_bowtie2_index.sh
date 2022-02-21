457caa6073d63f86c2e2b1447f1fee02c59097df
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-02-21 20:48:45 +0100

    update reference path

--- scripts_analysis/run_apps/run_bowtie2_index.sh
+++ scripts_analysis/run_apps/run_bowtie2_index.sh
@@ -7,18 +7,19 @@ REF_PATH="./Data/reference_genome_hg38"
 DATABASE="GRCh38"
 
 OUTPUT_PATH="./Analysis/Bowtie2"
-OUTPUT_GEN_DIR="${OUTPUT_PATH}/genome_dir_2"
+OUTPUT_GEN_DIR="${OUTPUT_PATH}/genome_dir"
 
 mkdir ${OUTPUT_PATH}
 mkdir ${OUTPUT_GEN_DIR}
+
+. ./data.config
 
 # create indexes    
 if [ -e ${OUTPUT_GEN_DIR}/${DATABASE}* ]; then
     echo "Database exists: ${DATABASE}"
 else
-    # ./bowtie2-2.4.5-linux-x86_64/bowtie2-build \
     bowtie2-build \
-        --threads 4 \
+        --threads ${n_threads} \
         ${REF_PATH}/${REFERENCE_GENOME_FILENAME} \
         ${OUTPUT_GEN_DIR}/${DATABASE} 
 fi

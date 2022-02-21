2422f618a47d2bc79d36877acac4f1de6db3a88d
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-02-21 20:51:07 +0100

    update data_config vars

--- scripts_analysis/run_apps/run_tophat2_alignment.sh
+++ scripts_analysis/run_apps/run_tophat2_alignment.sh
@@ -6,14 +6,15 @@ TOPHAT_DIR="./Analysis/Tophat2"
 
 rm -r ${TOPHAT_DIR}
 
-INPUT_FASTQ_PATH="./Analysis/Trimmomatic_2"
+INPUT_FASTQ_PATH="./Analysis/Trimmomatic"
 DATABASE="GRCh38"
 INPUT_PATH_GEN_DIR="./Analysis/Bowtie2/genome_dir"
 
 TOPHAT_DIR_ALING="${TOPHAT_DIR}/alignment"
-rm -r ${TOPHAT_DIR}
 mkdir ${TOPHAT_DIR}
 mkdir ${TOPHAT_DIR_ALING}
+
+. ./data.config
 
 # alignment
 for file in $(ls $INPUT_FASTQ_PATH)
@@ -24,11 +25,11 @@ do
         echo "Directory exists: ${SAMPLE%.*}"
     else
         echo "Alignment of sample: ${SAMPLE}..."
-        # tophat2 \
-        ./Older/tophat-2.1.1.Linux_x86_64/tophat2 \
+        # ./Older/tophat-2.1.1.Linux_x86_64/tophat2 \
+        tophat2 \
             --output-dir ${TOPHAT_DIR_ALING}/${SAMPLE%.*} \
-            --library-type fr-firststrand \
-            --num-threads 4 \
+            --library-type ${tophat2_library_type} \
+            --num-threads ${n_threads} \
             --max-multihits 1 \
             ${INPUT_PATH_GEN_DIR}/${DATABASE} \
             ${INPUT_FASTQ_PATH}/${SAMPLE} 

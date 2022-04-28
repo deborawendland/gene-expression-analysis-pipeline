7a6bdfbf18513b4e51106b2e3bf8ac14fbcd490e
Debora Wendland Amorim <deborawendland@gmail.com>
2022-04-28 16:32:37 +0200

    ungz reference genomes

--- scripts_analysis/data/download_reference_genome.sh
+++ scripts_analysis/data/download_reference_genome.sh
@@ -2,6 +2,12 @@
 
 mkdir ./Data/reference_genome_hg38
 
+echo "\nDownloading reference genomes"
+
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.26_GRCh38/GO_TO_CURRENT_VERSION/GCF_000001405.39_GRCh38.p13_genomic.fna.gz -P ./Data/reference_genome_hg38
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.26_GRCh38/GO_TO_CURRENT_VERSION/GCF_000001405.39_GRCh38.p13_genomic.gff.gz -P ./Data/reference_genome_hg38
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.26_GRCh38/GO_TO_CURRENT_VERSION/GCF_000001405.39_GRCh38.p13_genomic.gtf.gz -P ./Data/reference_genome_hg38
+
+echo "Unpacking .gz reference genomes"
+
+gzip -d ./Data/reference_genome_hg38/GCF_000001405.39_GRCh38.p13_genomic.gtf.gz
+gzip -d ./Data/reference_genome_hg38/GCF_000001405.39_GRCh38.p13_genomic.fna.gz

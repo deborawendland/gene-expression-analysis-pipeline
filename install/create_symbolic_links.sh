e2096aae1890c5617a87ee3c0dd55de37375c443
Debora Wendland Amorim <deborawendland@gmail.com>
2022-04-28 16:31:44 +0200

    update parameters

--- install/create_symbolic_links.sh
+++ install/create_symbolic_links.sh
@@ -1,18 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 link_path="/usr/bin"
 installations_path="install/installations"
-config_file="./data.config"
 
 bowtie2="${installations_path}/bowtie2"
 seqtk="${installations_path}/seqtk"
 tophat2="${installations_path}/tophat2"
 
-. ${config_file}
+pipeline_absolute_path=$1
 
-ln -s ${pipeline_absolute_path}/${seqtk}/seqtk ${link_path}/seqtk
-ln -s ${pipeline_absolute_path}/${tophat2}/tophat2 ${link_path}/tophat2
-ln -s ${pipeline_absolute_path}/${bowtie2}/bowtie2 ${link_path}/bowtie2
-ln -s ${pipeline_absolute_path}/${bowtie2}/bowtie2-align ${link_path}/bowtie2-align
-ln -s ${pipeline_absolute_path}/${bowtie2}/bowtie2-inspect ${link_path}/bowtie2-inspect
-ln -s ${pipeline_absolute_path}/${bowtie2}/bowtie2-build ${link_path}/bowtie2-build
+ln -sf ${pipeline_absolute_path}/${seqtk}/seqtk ${link_path}/seqtk
+ln -sf ${pipeline_absolute_path}/${tophat2}/tophat2 ${link_path}/tophat2
+ln -sf ${pipeline_absolute_path}/${tophat2}/tophat2/samtools_0.1.18 ${link_path}/samtools_0.1.18
+ln -sf ${pipeline_absolute_path}/${bowtie2}/bowtie2 ${link_path}/bowtie2
+ln -sf ${pipeline_absolute_path}/${bowtie2}/bowtie2-align ${link_path}/bowtie2-align
+ln -sf ${pipeline_absolute_path}/${bowtie2}/bowtie2-inspect ${link_path}/bowtie2-inspect
+ln -sf ${pipeline_absolute_path}/${bowtie2}/bowtie2-build ${link_path}/bowtie2-build

f005f53df052ce7b78a10e59f0201f5b3b8de665
Debora Wendland Amorim <deborawendland@gmail.com>
2022-04-28 17:44:16 +0200

    uncomment scripts

--- local_run_analysis.sh
+++ local_run_analysis.sh
@@ -1,9 +1,8 @@
 N_THREADS=8
 TOPHAT_LIBRARY_TYPE=fr-firststrand
 
-echo "\nChecking sample files..."
-check_samples=`ls -1 ./Data/samples/*.fastq 2>/dev/null | wc -l`
-if [ $check_samples == 0 ]; then 
+echo "Checking sample files..."
+if [ $(ls -1 ./Data/samples/*.fastq 2>/dev/null | wc -l) = 0 ]; then 
     echo "... No .fastq files in ./Data/samples/, please add the files to continue with the analysis"  
     exit 1
 else
@@ -29,15 +28,14 @@ else
 fi
 
 echo "\nChecking reference files..."
-check_ref_fna=`ls -1 ./Data/reference_genome_hg38/*.fna 2>/dev/null | wc -l`
-if [ $check_ref_fna == 0 ]; then 
+if [ $(ls -1 ./Data/reference_genome_hg38/*.fna 2>/dev/null | wc -l) = 0 ]; then 
     echo "... No .fna reference file in ./Data/reference_genome_hg38/, please add the file to continue with the analysis"  
     exit 1
 else
     echo "... .fna reference file - OK"
 fi
-check_ref_gtf=`ls -1 ./Data/reference_genome_hg38/*.gtf 2>/dev/null | wc -l`
-if [ $check_ref_fna == 0 ]; then 
+
+if [ $(ls -1 ./Data/reference_genome_hg38/*.gtf 2>/dev/null | wc -l) = 0 ]; then 
     echo "... No .gtf reference file in ./Data/reference_genome_hg38/, please add the file to continue with the analysis"  
     exit 1
 else
@@ -49,13 +47,13 @@ mkdir Analysis
 
 # sudo sh ./scripts_analysis/data/run_metadata.sh
 
-#sudo sh ./scripts_analysis/run_apps/run_fastqc.sh $N_THREADS
-#sudo sh ./scripts_analysis/run_apps/run_seqtk.sh $N_THREADS
-#sudo sh ./scripts_analysis/run_apps/run_trimmomatic.sh $N_THREADS
+sudo sh ./scripts_analysis/run_apps/run_fastqc.sh $N_THREADS
+sudo sh ./scripts_analysis/run_apps/run_seqtk.sh $N_THREADS
+sudo sh ./scripts_analysis/run_apps/run_trimmomatic.sh $N_THREADS
 
-#sudo sh ./scripts_analysis/run_apps/run_bowtie2_index.sh $N_THREADS
-#   sudo sh ./scripts_analysis/run_apps/run_tophat2_alignment.sh $N_THREADS $TOPHAT_LIBRARY_TYPE
-#sudo sh ./scripts_analysis/run_apps/run_samtools.sh $N_THREADS
+sudo sh ./scripts_analysis/run_apps/run_bowtie2_index.sh $N_THREADS
+sudo sh ./scripts_analysis/run_apps/run_tophat2_alignment.sh $N_THREADS $TOPHAT_LIBRARY_TYPE
+sudo sh ./scripts_analysis/run_apps/run_samtools.sh $N_THREADS
 
-#sudo sh ./scripts_analysis/run_apps/run_feature_counts.sh 
+sudo sh ./scripts_analysis/run_apps/run_feature_counts.sh 
 sudo sh ./scripts_analysis/run_apps/run_deseq2.sh 

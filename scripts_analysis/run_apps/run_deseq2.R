532841f9f807b05e680a81065926e0167d49fa3b
Debora Wendland Amorim <deborawendland@gmail.com>
2022-04-28 16:30:52 +0200

    update metadata

--- scripts_analysis/run_apps/run_deseq2.R
+++ scripts_analysis/run_apps/run_deseq2.R
@@ -11,7 +11,7 @@ require(dplyr)
 # DESEQ2
 output.path <- "./Analysis/DESeq2"
 
-metadata <- read.csv("./Data/samples/metadata.csv", 
+metadata <- read.csv("./Data/samples/metadata/metadata.csv", 
                 row.names=1)
 fc <- read.csv("./Analysis/FeatureCounts/counts_gtf.csv", 
                 row.names=1)
@@ -50,7 +50,7 @@ head(res)
 write.table(
   x=data.frame(res,
     stringsAsFactors=FALSE),
-  file="./Analysis/DESeq2/results_gtf.csv",
+  file="./Analysis/DESeq2/results-gtf.csv",
   quote=FALSE,
   sep=",",
   row.names=FALSE)
@@ -60,15 +60,22 @@ library(DESeq2)
 #rlog transformation
 rld <- rlog(dds, blind = F) 
 head(assay(rld), ) 
-write.csv(assay(rld), file="./Analysis/DESeq2/rlog_values.csv") 
+write.csv(assay(rld), file="./Analysis/DESeq2/rlog-values.csv") 
 
 #heatmaps
-heatmap.2(assay(rld), scale="row",trace="none", dendrogram="both", col = heat.colors(n = 75), margins = c(8,8)) #funcao para heatmap completo
+#heatmap.2(assay(rld), scale="row",trace="none", dendrogram="both", col = heat.colors(n = 75), margins = c(8,8)) #funcao para heatmap completo
 
 topVarGenes <- head( order( rowVars( assay(rld) ), decreasing=TRUE ), 50 ) #pular funcao se quiser heatmap de todos os genes, alterar o 50 para o numero de genes no ranking
+topVarGenes
+pdf("./Analysis/DESeq2/heatmap-top-var-genes.pdf", height=10, width=10)
 heatmap.2( assay(rld)[ topVarGenes, ], scale="row",trace="none", dendrogram="both",col = redgreen(75)) #pular funcao se quiser heatmap de todos os genes
+dev.off()
 
 ##distancia entre amostras
 sampleDists <- as.matrix(dist(t(assay(rld))))
 head(sampleDists)
+write.csv(sampleDists, file="./Analysis/DESeq2/sample-distances.csv") 
+
+pdf("./Analysis/DESeq2/heatmap-sample-distances.pdf", height=10, width=10)
 heatmap.2(as.matrix(sampleDists), scale="row",trace="none", dendrogram="both", col = heat.colors(n = 75), margins = c(8,8)) #funcao para heatmap completo
+dev.off()
